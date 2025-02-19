d5a8f138c48f24e4470673f1f8205a1a9b469390
yujin-Kim-98 <yujin123.kim@gmail.com>
2025-02-19 16:38:06 +0900

    refactor : LeetCode Contains Duplicate refactor

--- src/main/kotlin/leetcode/중복포함.kt
+++ src/main/kotlin/leetcode/중복포함.kt
@@ -3,12 +3,21 @@ package leetcode
 /**
  * https://leetcode.com/explore/featured/card/top-interview-questions-easy/92/array/646/
  * LeetCode - Top Interview Questions - Contains Duplicate
+ *
+ * 1.
+ * Runtime: 3902 ms
+ * Memory Usage: 52.3 MB
+ *
+ * 2.
+ * Runtime: 16 ms
+ * Memory Usage: 51.3 MB
+ *
  */
 fun main() {
 
-//    val nums = intArrayOf(1, 2, 3, 1)
+    val nums = intArrayOf(1, 2, 3, 1)
 
-    val nums = intArrayOf(1, 2, 3, 4)
+//    val nums = intArrayOf(1, 2, 3, 4)
 
 //    val nums = intArrayOf(1, 1, 1, 3, 3, 4, 3, 2, 4, 2)
 
@@ -20,9 +29,10 @@ class Solution {
 
     fun containsDuplicate(nums: IntArray): Boolean {
 
-        nums.forEach { num ->
-            val cnt = nums.count { it == num }
-            if (cnt > 1) {
+        val set = mutableSetOf<Int>()
+
+        nums.forEach {
+            if (!set.add(it)) {
                 return true
             }
         }
